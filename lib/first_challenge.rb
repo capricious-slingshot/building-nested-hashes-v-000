4b525f70f002982425f9d9143a90b9e128ea5ac0
Carden Z <ephemere@protonmail.com>
2019-01-31 03:40:10 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,5 @@
 def first_challenge
    epic_tragedy = {
-     montague: {[:patriarch, :matriarch, :hero, :hero_friends]},
-     capulet: {}
+     montague: {[:patriarch, :matriarch, :hero, :hero_friends]}
    }
 end
